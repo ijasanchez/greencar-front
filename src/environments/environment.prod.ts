2ec35ab55736262097ff6c1cb75e8a6c64c0092c
Luis Catano <lacatanoh@correo.udistrital.edu.co>
2019-04-21 21:35:47 -0500

    Firebase credentials removed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyCYbFgIVWC2g4QF40bISYk0LS3M3ldQssI',
-    authDomain: 'greencar-ffe80.firebaseapp.com',
-    databaseURL: 'https://greencar-ffe80.firebaseio.com',
-    projectId: 'greencar-ffe80',
-    storageBucket: 'greencar-ffe80.appspot.com',
-    messagingSenderId: '899451477314'
+    apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    authDomain: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    databaseURL: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    projectId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    storageBucket: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    messagingSenderId: 'XXXXXXXXXXXXXXXXXXXXXXXX'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyCYbFgIVWC2g4QF40bISYk0LS3M3ldQssI',
-    authDomain: 'greencar-ffe80.firebaseapp.com',
-    databaseURL: 'https://greencar-ffe80.firebaseio.com',
-    projectId: 'greencar-ffe80',
-    storageBucket: 'greencar-ffe80.appspot.com',
-    messagingSenderId: '899451477314'
+    apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    authDomain: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    databaseURL: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    projectId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    storageBucket: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    messagingSenderId: 'XXXXXXXXXXXXXXXXXXXXXXXX'
   }
 };
 
